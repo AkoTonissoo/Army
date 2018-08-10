2f913a7d4c25b92b5c7b43fbe2b6b57ce83033fc
Ako <ako.tonissoo@gmail.com>
2018-08-10 13:40:07 +0300

    creating empty schedule

--- army.js
+++ army.js
@@ -13,6 +13,7 @@ process.argv.forEach(function (val, index, array) {
  * creates array of tents asked by user
  * @example [[soldiers in tent1], [soldiers in tent2]]
  */
+
 const createTents = (data) => {
     let tentArray = [];
 
@@ -23,7 +24,7 @@ const createTents = (data) => {
     for (var i=0; i<nrOfTents; i++){
         tentArray.push([]);
         for (var a=0; a<nrOfSoldiers; a++){
-            
+
             //if there are more drivers needed
             if (a < nrOfDrivers){
                 tentArray[i].push({name: 'Soldier'+a, driver: 1, hours: 0, lastShift: 0});
@@ -34,4 +35,26 @@ const createTents = (data) => {
         }
     }
     return tentArray;
+};
+
+/**
+ * creates an empty schedule
+ * is grouped by tents and on every hour there will be empty slots for stovewatch and patrolling
+ */
+
+const createEmptySchedule = (hours, nrOfTents) => {
+
+    schedule = [];
+    for (var i=0; i<nrOfTents; i++){
+
+        schedule.push({tent:i, hours: []});
+        for (var a=0; a<hours; a++){
+            schedule[i].hours.push({hour:a, soldiers:{
+                stove: '',
+                patrol1: '',
+                patrol2: ''
+            }});
+        }
+    };
+    return schedule;
 };
